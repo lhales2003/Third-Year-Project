4ecaac05dae68fb5e2163d76cf44b03a3f5fa1ce
lhales2003 <Lukehales2003@gmail.com>
2024-12-13 15:56:50 +0000

    Added data

--- Main/Uploading.py
+++ Main/Uploading.py
@@ -1,18 +1,39 @@
 import geopandas as gpd
+import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 # Load the shapefile
-shapefile = gpd.read_file("Main/NUTS_Level_1_January_2018_FEB_in_the_United_Kingdom.shp")
+shapefile = gpd.read_file("Main/PCON_JULY_2024_UK_BSC.shp")
+csvfile = pd.read_csv("Main/foi20190080data.csv")
 
-shapefile.plot()
+print(shapefile.info())
+print(csvfile.info())
 
-crs = ccrs.PlateCarree()
+merged_data = shapefile.merge(csvfile, left_on='PCON24NM', right_on='PCON11NM')
 
+print(merged_data.info())
 
-crs_proj4 = crs.proj4_init
-shapefile_ae = shapefile.to_crs(crs_proj4)
+fig, ax = plt.subplots(figsize=(10, 10))
 
-shapefile_ae.plot()
+shapefile.plot(ax=ax, color='lightgrey', edgecolor='black')
+
+merged_data.plot(column='Mid-2018', ax=ax, cmap='OrRd')
+
+# Customize the plot
+plt.title('Map with Data from CSV')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
 plt.show()
+
+# shapefile.plot()
+
+# crs = ccrs.PlateCarree()
+
+
+# crs_proj4 = crs.proj4_init
+# shapefile_ae = shapefile.to_crs(crs_proj4)
+
+# shapefile_ae.plot()
+# plt.show()
