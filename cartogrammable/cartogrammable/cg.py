9f8e09623575d8dbb3bfa03b1c4d7c9314b701d1
lh880 <Lukehales2003@gmail.com>
2024-12-24 19:18:46 +0000

    Added ability to create a heatmap

--- cartogrammable/cartogrammable/cg.py
+++ cartogrammable/cartogrammable/cg.py
@@ -1,23 +1,51 @@
 import sys
 import geopandas as gpd
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class CartogramGenerator:
-    def __init__(self, shapefile, csv_file, shapefile_column, csv_file_column):
+    def __init__(self, shapefile, csv_file, statistic_column):
         self.map_borders = None
         self.map_data = None
+        self.statistic_column = None
         self.cartogram_type = None
+        self.merged_data = None
                     
-        self.load_data(shapefile, csv_file, shapefile_column, csv_file_column)
+        self.load_data(shapefile, csv_file, statistic_column)
     
-    def load_data(self, shapefile, csv_file, shapefile_column, csv_file_column):
-        try:
-            self.map_borders = gpd.read_file(shapefile)
-            self.map_data = pd.read_csv(csv_file)
-        except:
-            print("Error loading files, ensure file names and paths are correct and try again. Terminating program to avoid further errors")
+    def load_data(self, shapefile, csv_file, statistic_column):
+        if isinstance(shapefile, gpd.geodataframe.GeoDataFrame) and isinstance(csv_file, pd.core.frame.DataFrame):
+            self.map_borders = shapefile
+            self.map_data = csv_file
+            self.statistic_column = statistic_column
+        else:
+            print("Error loading files, ensure DataFrame(s) are of correct type and try again. Terminating program to avoid further errors")
             sys.exit(0)
         
+        self.merge_data()
+        
+    def merge_data(self):
+        shapefile_columns = list(self.map_borders.columns.values)
+        csv_file_columns = list(self.map_data.columns.values)
+        
+        merge_column = None
+        for column in shapefile_columns:
+            if column in csv_file_columns:
+                merge_column = column
+                pass
+        
+        if merge_column is None:
+            print("No columns in common, must have one column labelled the same in order to merge DataFrames. Please modify the Pandas file and try again.")
+            sys.exit(0)
+        
+        average_statistic = self.map_data[self.statistic_column].mean()
+        
+        
+        self.merged_data = self.map_borders.merge(self.map_data, left_on=merge_column, right_on=merge_column)
+
+        if self.merged_data[merge_column].count() == 0:
+            print("No values exist in DataFrame after merge. Please try another column")
+            sys.exit(0)
         
     def shapefile_info(self):
         try:
@@ -31,5 +59,28 @@ class CartogramGenerator:
             return self.map_data.info()
         except:
             print("CSV file not present in CartogramGenerator object. Terminating program to avoid further errors")
+    
+    def modify_shapefile(self, new_shapefile):
+        if isinstance(new_shapefile, gpd.geodataframe.GeoDataFrame):
+            self.map_borders = new_shapefile
+        else:
+            print("Error loading file, ensure DataFrame is of correct type and try aagin. Terminating program to avoid further errors.")
         
+        self.merge_data()
+           
+    def modify_csv_file(self, new_csv_file):
+        if isinstance(new_csv_file, pd.core.frame.DataFrame):
+            self.map_borders = new_csv_file
+        else:
+            print("Error loading file, ensure DataFrame is of correct type and try aagin. Terminating program to avoid further errors.")
+        
+        self.merge_data()
+        
+    def plot_heatmap(self):
+        fig, ax = plt.subplots(figsize= (10,10))
+        self.map_borders.plot(ax=ax, color="lightgray", edgecolor="black")
+        self.merged_data.plot(column=self.statistic_column, ax=ax, cmap="OrRd")
+        plt.title("Loaded Map with Merged Data")
+        plt.xlabel("Longitude")
+        plt.ylabel("Latitude")
         
--- cartogrammable/cartogrammable/dynamic_test_env.py
+++ cartogrammable/cartogrammable/dynamic_test_env.py
@@ -1,6 +1,27 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
 from cg import CartogramGenerator
 
-generator = CartogramGenerator()
+test_shapefile = gpd.read_file("cartogrammable/cartogrammable/PCON_JULY_2024_UK_BUC.shp")
+test_csv_file = pd.read_csv("cartogrammable/cartogrammable/foi20190080data.csv")
 
-print(generator.shapefile_info())
-print("test")
+print(test_shapefile.head())
+print(test_shapefile.info())
+print(test_csv_file.head())
+print(test_csv_file.info())
+
+print(type(test_shapefile)) 
+print(type(test_csv_file))
+print(isinstance(test_shapefile, gpd.geodataframe.GeoDataFrame))
+print(isinstance(test_csv_file, pd.core.frame.DataFrame))
+
+print(list(test_shapefile.columns.values))
+print(list(test_csv_file.columns.values))
+
+test_csv_file["PCON24NM"] = test_csv_file["PCON11NM"]
+test_csv_file["Mid-2018"] = test_csv_file["Mid-2018"].str.replace(",", "").astype("int64")
+cg = CartogramGenerator(test_shapefile, test_csv_file, "Mid-2018")
+
+cg.plot_heatmap()
+plt.show()
